8561f968724e885dc80590efdd7ed07f2bcb6ddf
Scott Rosenberg <scott@terasky.com>
2025-02-12 20:33:33 +0200

    param as id

--- config/external_name.go
+++ config/external_name.go
@@ -10,8 +10,8 @@ import "github.com/crossplane/upjet/pkg/config"
 // provider.
 var ExternalNameConfigs = map[string]config.ExternalName{
 	// Import requires using a randomly generated ID from provider: nl-2e21sda
-	"aerospike_user": config.ParameterAsIdentifier("user_name"),
 	"aerospike_role": config.ParameterAsIdentifier("role_name"),
+	"aerospike_user": config.ParameterAsIdentifier("user_name"),
 }
 
 // ExternalNameConfigurations applies all external name configs listed in the
